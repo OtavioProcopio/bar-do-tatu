864614413fa65dc2cdc4139967d35147c55af30b
Otavio Procopio <tavinprocopio@gmail.com>
2025-01-07 11:31:32 -0300

    Produto Entity

---
+++ src/main/java/tatu/bar/backend/entity/Produto.java
@@ -0,0 +1,36 @@
+package tatu.bar.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Produtos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private String categoria;
+
+    private int quantidadeEstoque;
+
+    private double precoDeCusto;
+
+    private double precoDeVenda;
+}
